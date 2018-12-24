5cca40a110dd9bba901fcc129ef755b3de88b1d7
Anna Morozova <Anna.Morozova@t-systems.com>
2018-12-24 12:25:24 +0300

    observable example

--- src/code.ts
+++ src/code.ts
@@ -1,3 +1,31 @@
-import * as Rx from "rxjs/Observable";
+import { Observable } from "rxjs/Observable";
 
-console.log(Rx);
+var observable = Observable.create((observer: any) => {
+    try {
+        observer.next('hey');
+        observer.next('how are you?');
+        setInterval(() => {
+           observer.next('fine')
+        }, 2000);
+        // observer.complete();
+        // observer.next('this will not send');
+    } catch(err) {
+        observer.error(err);
+    }
+});
+
+var observer = observable.subscribe(
+    (x: any) => addItem(x),
+    (error: any) => addItem(error),
+    () => addItem('Completed'));
+
+setTimeout(() => {
+    observer.unsubscribe();
+}, 6001);
+
+function addItem(val: any) {
+    var node = document.createElement("li");
+    var textnode = document.createTextNode(val);
+    node.appendChild(textnode);
+    document.getElementById("output").appendChild(node);
+}
